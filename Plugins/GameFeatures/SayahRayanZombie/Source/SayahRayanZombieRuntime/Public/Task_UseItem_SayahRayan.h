04f6b729bed1aaa756a61098a66ebc7350573cfb
Rayan <rayan.sayah@student.howest.be>
2026-06-05 19:14:27 +0200

    Header of use item

---
+++ Plugins/GameFeatures/SayahRayanZombie/Source/SayahRayanZombieRuntime/Public/Task_UseItem_SayahRayan.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "Task_UseItem_SayahRayan.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAYAHRAYANZOMBIERUNTIME_API UTask_UseItem_SayahRayan : public UBTTaskNode
+{
+	GENERATED_BODY()
+public:
+	UTask_UseItem_SayahRayan();
+	virtual  EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
