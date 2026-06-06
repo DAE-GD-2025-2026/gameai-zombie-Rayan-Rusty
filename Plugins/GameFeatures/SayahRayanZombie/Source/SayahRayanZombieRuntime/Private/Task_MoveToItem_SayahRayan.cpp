9c52ad1f10b30e1f43be094334c4f8e94f31cb5b
Rayan <rayan.sayah@student.howest.be>
2026-06-06 21:42:58 +0200

    Move to item prioritizes items depending on its health or stamina

--- Plugins/GameFeatures/SayahRayanZombie/Source/SayahRayanZombieRuntime/Private/Task_MoveToItem_SayahRayan.cpp
+++ Plugins/GameFeatures/SayahRayanZombie/Source/SayahRayanZombieRuntime/Private/Task_MoveToItem_SayahRayan.cpp
@@ -4,6 +4,7 @@
 #include "Task_MoveToItem_SayahRayan.h"
 
 #include "AIController.h"
+#include "NavigationSystem.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "Items/BaseItem.h"
 #include "Items/Food.h"
@@ -18,6 +19,7 @@ UTask_MoveToItem_SayahRayan::UTask_MoveToItem_SayahRayan()
 
 EBTNodeResult::Type UTask_MoveToItem_SayahRayan::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
+	//TODO empty gun , check inventory fix snapping by using project navmesh
 	AAIController* Controller = OwnerComp.GetAIOwner();
 	if (!Controller) return EBTNodeResult::Failed;
 
@@ -30,38 +32,68 @@ EBTNodeResult::Type UTask_MoveToItem_SayahRayan::ExecuteTask(UBehaviorTreeCompon
 	UHealthComponent* HealthComp = Survivor->FindComponentByClass<UHealthComponent>();
 	UStaminaComponent* StaminaComp = Survivor->FindComponentByClass<UStaminaComponent>();
 
-	bool bNeedsHealth = HealthComp && HealthComp->GetHealth() < 50.f;
-	bool bNeedsStamina = StaminaComp && StaminaComp->GetCurrentStamina() < 50.f;
 
+	float HealthPercent =  HealthComp->GetHealth() / HealthComp->GetMaxHealth();
+	float StaminePercent = StaminaComp->GetCurrentStamina() / StaminaComp->GetMaxStamina();
+
+	bool StaminaMoreUrgent = StaminePercent < HealthPercent;
 	ABaseItem* TargetItem = nullptr;
 
-	if (bNeedsHealth)
-	{
-		if (AMedkit* Medkit = Cast<AMedkit>(Board->GetValueAsObject(FName("Medkit"))))
-			TargetItem = Medkit;
-	}
-	else if (bNeedsStamina)
+
+	if (StaminaMoreUrgent)
 	{
 		if (AFood* Food = Cast<AFood>(Board->GetValueAsObject(FName("Food"))))
+		{
 			TargetItem = Food;
+		}
+		else if (AMedkit* Medkit = Cast<AMedkit>(Board->GetValueAsObject(FName("Medkit"))))
+		{
+			TargetItem = Medkit;
+		}
+	}
+	else
+	{
+		if (AMedkit* Medkit = Cast<AMedkit>(Board->GetValueAsObject(FName("Medkit"))))
+		{
+			TargetItem = Medkit;
+		}
+		else if(AFood* Food = Cast<AFood>(Board->GetValueAsObject(FName("Food"))))
+		{
+			TargetItem = Food;
+		}
 	}
 
 	if (!TargetItem)
 	{
-		if (AMedkit* Medkit = Cast<AMedkit>(Board->GetValueAsObject(FName("Medkit"))))
-			TargetItem = Medkit;
-		else if (AFood* Food = Cast<AFood>(Board->GetValueAsObject(FName("Food"))))
-			TargetItem = Food;
-		else if (APistol* Pistol = Cast<APistol>(Board->GetValueAsObject(FName("Handgun"))))
+		if (APistol* Pistol = Cast<APistol>(Board->GetValueAsObject(FName("Handgun"))))
 			TargetItem = Pistol;
 		else if (AShotgun* Shotgun = Cast<AShotgun>(Board->GetValueAsObject(FName("Shotgun"))))
 			TargetItem = Shotgun;
 	}
 
-
 	
 	if (!TargetItem) return EBTNodeResult::Failed;
 
-	Controller->MoveToActor(TargetItem, 1.f, false);
+	float Dist = FVector::Dist(Survivor->GetActorLocation(), TargetItem->GetActorLocation());
+
+	if (Dist > 100.f)
+	{
+		FVector TargetLocation = TargetItem->GetActorLocation();
+
+		// Project onto navmesh
+		UNavigationSystemV1* NavSys = UNavigationSystemV1::GetCurrent(Survivor->GetWorld());
+		FNavLocation NavLocation;
+		if (NavSys && NavSys->ProjectPointToNavigation(TargetLocation, NavLocation))
+		{
+			TargetLocation = NavLocation.Location;
+		}
+
+		Controller->MoveToLocation(TargetLocation, 20.f, false);
+		return EBTNodeResult::Succeeded;
+	}
+
+	
+
 	return EBTNodeResult::Succeeded;
+
 }
